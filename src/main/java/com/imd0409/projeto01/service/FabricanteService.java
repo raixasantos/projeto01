8243d030b21cb1b6db7d4fa7610c44dc90ec2a7a
Raíssa Santos <raissa.santos.095@ufrn.edu.br>
2022-10-04 01:27:30 -0300

    feat: funcionalidades de editar, apagar Fabricante

--- src/main/java/com/imd0409/projeto01/service/FabricanteService.java
+++ src/main/java/com/imd0409/projeto01/service/FabricanteService.java
@@ -1,6 +1,7 @@
 package com.imd0409.projeto01.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -10,5 +11,9 @@ import com.imd0409.projeto01.model.Fabricante;
 public interface FabricanteService {
 
     public List<Fabricante> getListaFabricante();
+    public Optional<Fabricante> getFabricanteById(Integer id);
+    public void salvarFabricante(Fabricante fabricante);
+    public void apagarFabricante(Integer id);
+    public void editarFabricante(Fabricante fabricante); 
     
 }
