ffcb677cadf050d81b67502cac6f2e0b688c886b
Raíssa Santos <raissa.santos.095@ufrn.edu.br>
2022-11-07 09:18:54 -0300

    feat: requisições da entidade Carteira
    Requisições da criar e recuperar da entidade Carteira

--- src/main/java/com/imd0409/vacinacaobovino/service/CarteiraServiceImpl.java
+++ src/main/java/com/imd0409/vacinacaobovino/service/CarteiraServiceImpl.java
@@ -4,33 +4,68 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.imd0409.vacinacaobovino.model.Bovino;
 import com.imd0409.vacinacaobovino.model.Carteira;
+import com.imd0409.vacinacaobovino.repository.BovinoRepository;
 import com.imd0409.vacinacaobovino.repository.CarteiraRepository;
+import com.imd0409.vacinacaobovino.rest.dto.NovaCarteiraDTO;
 
 @Component
-public class CarteiraServiceImpl implements CarteiraService{
+public class CarteiraServiceImpl implements CarteiraService {
+
+    @Autowired
+    BovinoRepository bovinoRepository;
 
     @Autowired
     CarteiraRepository carteiraRepository;
 
     @Override
-    public List<Carteira> getListaCarteira() {
+    public List<Carteira> obterListaCarteira() {
         return carteiraRepository.findAll();
     }
 
     @Override
-    public Carteira salvarCarteira(Bovino bovino) {
-        Carteira carteira = new Carteira();
-        carteira.setBovino(bovino);
-        return carteiraRepository.save(carteira);
+    public Integer salvarCarteira(NovaCarteiraDTO carteira) {
+        Integer idBovinoRecebido = carteira.getIdBovino();
+        // mudar modo de envio de erro
+        Bovino bovino = bovinoRepository
+                .findById(idBovinoRecebido)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Bovino não encontrado."));
+
+        Boolean existeCarteiraComIdRecebido = carteiraRepository
+                .encontrarPorIdBovino(idBovinoRecebido)
+                .isPresent();
+        if (existeCarteiraComIdRecebido) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_ACCEPTABLE,
+                    "Carteira com o id do bovino " + idBovinoRecebido + " já existe!"
+            );
+        }
+
+        Carteira novaCarteira = new Carteira();
+        novaCarteira.setBovino(bovino);
+        carteiraRepository.save(novaCarteira);
+        return novaCarteira.getId();
     }
 
     @Override
-    public Optional<Carteira> getCarteiraByIdBovino(Integer id) {
-        return carteiraRepository.encontrarPorIdBovino(id);
+    public Carteira obterCarteiraPorIdBovino(Integer idBovino) {
+        Optional<Carteira> carteira = carteiraRepository.encontrarPorIdBovino(idBovino);
+        Boolean existeCarteiraComIdRecebido = carteira.isPresent();
+        if (!existeCarteiraComIdRecebido) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    "Carteira do bovino de id " + idBovino + " não encontrada!"
+            );
+        }
+
+        // converter: bovino e aplicaçoes     
+
+        return carteira.get(); // retornar 
     }
-    
+
 }
